ef6dc12447e40387b94d3b4231d9acbcdfd515e2
Lisa Min <lisajmin@gmail.com>
2017-02-07 22:06:44 -0800

    Checkpoint 5 - Set Username

---
+++ app/scripts/controllers/UserCtrl.js
@@ -0,0 +1,17 @@
+( function() {
+    function UserCtrl($scope, $cookies, $uibModalInstance) {
+        $scope.setUsername = function(username) {
+            if (username !== undefined) {
+            $cookies.put('blocChatCurrentUser', username);
+            $uibModalInstance.close();
+            } else {
+                alert("Please enter a username");
+            }
+        }
+
+    }
+
+    angular
+        .module('blocChat')
+        .controller('UserCtrl', ['$scope', '$cookies', '$uibModalInstance',  UserCtrl]);
+})();
